97ec258209a0a347004797f609e28f6b44f786f6
leslie <sarybell28@gmail.com>
2023-01-10 16:28:42 -0500

    create-new-category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateNewCategoryComponent } from './Componente/create-new-category/create-new-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateNewCategoryComponent
   ],
   imports: [
     BrowserModule
